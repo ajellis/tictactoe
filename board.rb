f6f13693b53f06f61d0bf8c21b74589c8410a886
Anthony Ellis <ajellis1989@gmail.com>
2015-10-27 04:46:02 -0400

    Refactors some code

--- board.rb
+++ board.rb
@@ -1,6 +1,28 @@
-def initialize
-  @spaces = Array.new(9)
-end
+class Board
+
+  def initialize
+    @board =  [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "]
+    ]
+  end
+
+
+  def print_board
+    (0..2).each do |r|
+      print " "
+      (0..2).each do |c|
+        print @board[r][c]
+        print " | " unless c == 2
+      end
+      print "\n"
+      print " ---------\n" unless r == 2
+    end
+    print "\n"
+  end
+
+
 
 winning_positions = [
   ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"],
@@ -8,43 +30,24 @@ winning_positions = [
   ["1", "5", "9"], ["3", "5", "7"]
 ]
 
-positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-board_array =
-  [["1", "2", "3"],
-   ["4", "5", "6"],
-   ["7", "8", "9"]
 
-def intro_text
+
   puts "Welcome to Tic Tac Toe! Choose a position!"
-  #
-  # puts "Valid locations are A1 (top-left), A2 (top-center), A3 (top-right), B1 (middle-left), B2 (middle-center), B3 (middle-right),
-  #     C1 (bottom-left), C2 (bottom-center), and C3 (bottom-right)"
-end
+  puts " 1 | 2 | 3 "
+  puts "-----------"
+  puts " 4 | 5 | 6 "
+  puts "-----------"
+  puts " 7 | 8 | 9 "
 
- 0 | 1 | 2
-----------
- 3 | 4 | 5
-----------
- 6 | 7 | 8
-
- @spaces = Array.new(9)
-
-intro_text
-empty_board
-
-loop do
-  if counter >= 9
-    puts "Game Over! It's a Tie!"
-    break
-  else
     puts "Enter A Location!"
     location = gets.chomp.upcase
-  end
+    positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
   if positions.include?(location)
     puts "Good Choice!"
-    counter +=1
   else
     puts "That's not a valid location. Choose another"
   end
+
+
 end
