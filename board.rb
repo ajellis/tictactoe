153b5c259e8741091be3b7328b95a667878018f3
Anthony Ellis <ajellis1989@gmail.com>
2015-10-27 04:29:47 -0400

    Adds experimntal board.

--- board.rb
+++ board.rb
@@ -1,43 +1,50 @@
-@counter = 0
-positions = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"]
-
-def empty_board
-  2.times do
-    puts "__|__|__"
-  end
-  puts "  |  |"
+def initialize
+  @spaces = Array.new(9)
 end
 
+winning_positions = [
+  ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"],
+  ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"],
+  ["1", "5", "9"], ["3", "5", "7"]
+]
 
+positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+board_array =
+  [["1", "2", "3"],
+   ["4", "5", "6"],
+   ["7", "8", "9"]
 
 def intro_text
   puts "Welcome to Tic Tac Toe! Choose a position!"
-
-  puts "Valid locations are A1 (top-left), A2 (top-center), A3 (top-right), B1 (middle-left), B2 (middle-center), B3 (middle-right),
-      C1 (bottom-left), C2 (bottom-center), and C3 (bottom-right)"
+  #
+  # puts "Valid locations are A1 (top-left), A2 (top-center), A3 (top-right), B1 (middle-left), B2 (middle-center), B3 (middle-right),
+  #     C1 (bottom-left), C2 (bottom-center), and C3 (bottom-right)"
 end
 
-# def get_location
-#   loop do
-#     if counter >= 8
-#       break
-#     else
-#       puts "Enter A Location!"
-#
-#       @location = gets.chomp.upcase
-#
-#     end
-#     if positions.!include?(@location)
-#       puts "That's not a valid position."
-#     else
-#       puts "Good Choice!"
-#       @counter +=1
-#     end
-#   end
-# end
+ 0 | 1 | 2
+----------
+ 3 | 4 | 5
+----------
+ 6 | 7 | 8
 
+ @spaces = Array.new(9)
 
 intro_text
 empty_board
-# get_location
+
+loop do
+  if counter >= 9
+    puts "Game Over! It's a Tie!"
+    break
+  else
+    puts "Enter A Location!"
+    location = gets.chomp.upcase
+  end
+  if positions.include?(location)
+    puts "Good Choice!"
+    counter +=1
+  else
+    puts "That's not a valid location. Choose another"
+  end
+end
