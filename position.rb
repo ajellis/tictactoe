4c76ca795672fe79c5c5569f7cf9b983b0ea58d1
Anthony Ellis <ajellis1989@gmail.com>
2015-10-27 11:47:26 -0400

    Finally got it to tell me if I win.

--- position.rb
+++ position.rb
@@ -3,7 +3,7 @@ class Position
 
   def initialize
     @counter = 0
-    @accepted_entry = []
+    @accepted_entries = []
     @positions = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
     @board =  [
       [" ", " ", " "],
@@ -45,23 +45,29 @@ class Position
     @board[row(location)][col(location)] = "X"
   end
 
+
   def board_spot
-    while @counter <= 8
-    # while @board.any? { |row| row.include?(" ") }
-      puts "Enter a position!"
-      location = gets.chomp.upcase
-      if @accepted_entry.include?(location)
-        puts "Nah, already taken bro. Pick another location."
-      elsif @positions.include?(location)
-      @accepted_entry << location
-      @counter += 1
-      update_board(location)
-      print_board
-      elsif puts "That's not a valid location. Choose another."
-      end
-    end
-    puts "Game Over"
+   while @counter <= 8
+   # while @board.any? { |row| row.include?(" ") }
+     puts "Enter a position!"
+     location = gets.chomp.upcase
+     if @accepted_entries.include?(location)
+       puts "Nah, already taken bro. Pick another location."
+     elsif @positions.include?(location)
+     @accepted_entries << location
+     @counter += 1
+     update_board(location)
+     print_board
+     @winning_positions.each do |array|
+        return puts "YOU WIN!" if @accepted_entries.include?(array[0]) && @accepted_entries.include?(array[1]) && @accepted_entries.include?(array[2])
+     end
+     elsif puts "That's not a valid location. Choose another."
+     end
+   end
+   puts "Game Over. It's a Tie!"
   end
+
+
 
 
 
