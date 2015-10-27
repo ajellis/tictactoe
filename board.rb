1720b253c77e3208c513759597b27286fae20f69
Anthony Ellis <ajellis1989@gmail.com>
2015-10-27 05:55:20 -0400

    Will not allow use of the same location more than once.

--- board.rb
+++ board.rb
@@ -1,54 +1,69 @@
-class Board
 
-  def initialize
     @board =  [
       [" ", " ", " "],
       [" ", " ", " "],
       [" ", " ", " "]
     ]
-  end
 
+    @counter = 0
+
+    @accepted_entry = []
 
   def print_board
-    (0..2).each do |r|
+    (0...2).each do |row|
       print " "
-      (0...2).each do |c|
-        print @board[r][c]
+      (0...2).each do |col|
+        print @board[row][col]
         print " | "
       end
       print "\n"
-      print " ---------\n" unless r == 2
+      print " ---------\n"
     end
     print "\n"
   end
 
-
-winning_positions = [
-  ["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"],
-  ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"],
-  ["A1", "B2", "C3"], ["A3", "B2", "C1"]
-]
-
+  winning_positions = [
+    ["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"],
+    ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"],
+    ["A1", "B2", "C3"], ["A3", "B2", "C1"]
+  ]
 
 
 
-  puts ""
-  puts "Welcome to Tic Tac Toe! Choose a position!"
-  puts ""
-  puts " A1 | A2 | A3 "
-  puts "--------------"
-  puts " B1 | B2 | B3 "
-  puts "--------------"
-  puts " C1 | C2 | C3 "
-
-
-  location = gets.chomp.upcase
-  positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  if positions.include?(location)
-    puts "Good Choice!"
-  else
-    puts "That's not a valid location. Choose another"
+  def display_intro
+    puts ""
+    puts " A1 | A2 | A3 "
+    puts "--------------"
+    puts " B1 | B2 | B3 "
+    puts "--------------"
+    puts " C1 | C2 | C3 "
   end
 
-
+  def check_user
+    loop do
+      if @counter >= 9
+        puts "Game Over!"
+        break
+      else
+        puts ""
+        puts "Welcome to Tic Tac Toe! Choose a position!"
+        location = gets.chomp.upcase
+      end
+        positions = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+      if @accepted_entry.include?(location)
+        puts "Nah. Already Taken. Try Somewhere else."
+      elsif positions.include?(location)
+        puts "Good Choice!"
+        @counter +=1
+        @accepted_entry << location
+    else
+      puts "That's not a valid location. Choose another"
+    end
+  end
 end
+
+
+
+
+display_intro
+check_user
